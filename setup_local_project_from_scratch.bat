164f1484a691a3172fb7caeac951ede1f6f533c9
darMak <darekmakarewiczpraca@gmail.com>
2023-10-09 10:10:40 +0200

    initial configuration

---
+++ setup_local_project_from_scratch.bat
@@ -0,0 +1,11 @@
+@echo off
+rmdir /s /q .\venv
+
+python -m pip install --upgrade pip
+python -m pip install virtualenv
+python -m virtualenv venv
+
+@REM .\venv\Scripts\activate.bat
+venv\Scripts\Activate.ps1
+
+python -m pip install -r requirements.txt
