a6bfcbc216f4b6932e3183740ba57811ef06cc02
darMak <darekmakarewiczpraca@gmail.com>
2023-10-10 14:33:40 +0200

    add Flask with Docker

--- orm.py
+++ orm.py
@@ -1,8 +1,6 @@
-from sqlalchemy import (
-    Table, MetaData, Column, Integer, String, Date,
-    ForeignKey
-)
-from sqlalchemy.orm import relationship, registry
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column, Date, ForeignKey, Integer, MetaData, String, Table
+from sqlalchemy.orm import registry, relationship
 
 import model
 
@@ -11,36 +9,43 @@ mapper_registry = registry()
 metadata = MetaData()
 
 order_lines = Table(
-    'order_lines', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('sku', String(255)),
-    Column('qty', Integer, nullable=False),
-    Column('orderid', String(255)),
+    "order_lines",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("sku", String(255)),
+    Column("qty", Integer, nullable=False),
+    Column("orderid", String(255)),
 )
 
 batches = Table(
-    'batches', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('reference', String(255)),
-    Column('sku', String(255)),
-    Column('_purchased_quantity', Integer, nullable=False),
-    Column('eta', Date, nullable=True),
+    "batches",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("reference", String(255)),
+    Column("sku", String(255)),
+    Column("_purchased_quantity", Integer, nullable=False),
+    Column("eta", Date, nullable=True),
 )
 
 allocations = Table(
-    'allocations', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('orderline_id', ForeignKey('order_lines.id')),
-    Column('batch_id', ForeignKey('batches.id')),
+    "allocations",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("orderline_id", ForeignKey("order_lines.id")),
+    Column("batch_id", ForeignKey("batches.id")),
 )
 
 
 def start_mappers():
     lines_mapper = mapper_registry.map_imperatively(model.OrderLine, order_lines)
-    mapper_registry.map_imperatively(model.Batch, batches, properties={
-        '_allocations': relationship(
-            lines_mapper,
-            secondary=allocations,
-            collection_class=set,
-        )
-    })
+    mapper_registry.map_imperatively(
+        model.Batch,
+        batches,
+        properties={
+            "_allocations": relationship(
+                lines_mapper,
+                secondary=allocations,
+                collection_class=set,
+            )
+        },
+    )
--- repository.py
+++ repository.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 import abc
+
 import model
 
 
 class AbstractRepository(abc.ABC):
-
     @abc.abstractmethod
     def add(self, batch: model.Batch):
         raise NotImplementedError
@@ -13,9 +14,7 @@ class AbstractRepository(abc.ABC):
         raise NotImplementedError
 
 
-
 class SqlAlchemyRepository(AbstractRepository):
-
     def __init__(self, session):
         self.session = session
 
