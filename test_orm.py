aed50637d56e87dbbb35a46593b283a32bebcd9d
darMak <darekmakarewiczpraca@gmail.com>
2023-10-10 14:33:40 +0200

    add Flask with Docker

--- test_orm.py
+++ test_orm.py
@@ -1,13 +1,19 @@
-import model
+# -*- coding: utf-8 -*-
 from datetime import date
+
 from sqlalchemy import text
+
+import model
+
 
 def test_orderline_mapper_can_load_lines(session):
     session.execute(
-        text('INSERT INTO order_lines (orderid, sku, qty) VALUES '
-        '("order1", "RED-CHAIR", 12),'
-        '("order1", "RED-TABLE", 13),'
-        '("order2", "BLUE-LIPSTICK", 14)')
+        text(
+            "INSERT INTO order_lines (orderid, sku, qty) VALUES "
+            '("order1", "RED-CHAIR", 12),'
+            '("order1", "RED-TABLE", 13),'
+            '("order2", "BLUE-LIPSTICK", 14)'
+        )
     )
     expected = [
         model.OrderLine("order1", "RED-CHAIR", 12),
@@ -26,15 +32,15 @@ def test_orderline_mapper_can_save_lines(session):
     assert rows == [("order1", "DECORATIVE-WIDGET", 12)]
 
 
-
 def test_retrieving_batches(session):
     session.execute(
-        text('INSERT INTO batches (reference, sku, _purchased_quantity, eta)'
-        ' VALUES ("batch1", "sku1", 100, null)')
+        text("INSERT INTO batches (reference, sku, _purchased_quantity, eta)" ' VALUES ("batch1", "sku1", 100, null)')
     )
     session.execute(
-        text('INSERT INTO batches (reference, sku, _purchased_quantity, eta)'
-        ' VALUES ("batch2", "sku2", 200, "2011-04-11")')
+        text(
+            "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
+            ' VALUES ("batch2", "sku2", 200, "2011-04-11")'
+        )
     )
     expected = [
         model.Batch("batch1", "sku1", 100, eta=None),
@@ -45,17 +51,16 @@ def test_retrieving_batches(session):
 
 
 def test_saving_batches(session):
-    batch = model.Batch('batch1', 'sku1', 100, eta=None)
+    batch = model.Batch("batch1", "sku1", 100, eta=None)
     session.add(batch)
     session.commit()
-    rows = list(session.execute(
-        text('SELECT reference, sku, _purchased_quantity, eta FROM "batches"')
-    ))
-    assert rows == [('batch1', 'sku1', 100, None)]
+    rows = list(session.execute(text('SELECT reference, sku, _purchased_quantity, eta FROM "batches"')))
+    assert rows == [("batch1", "sku1", 100, None)]
+
 
 def test_saving_allocations(session):
-    batch = model.Batch('batch1', 'sku1', 100, eta=None)
-    line = model.OrderLine('order1', 'sku1', 10)
+    batch = model.Batch("batch1", "sku1", 100, eta=None)
+    line = model.OrderLine("order1", "sku1", 10)
     batch.allocate(line)
     session.add(batch)
     session.commit()
@@ -64,28 +69,20 @@ def test_saving_allocations(session):
 
 
 def test_retrieving_allocations(session):
-    session.execute(
-        text('INSERT INTO order_lines (orderid, sku, qty) VALUES ("order1", "sku1", 12)')
-    )
+    session.execute(text('INSERT INTO order_lines (orderid, sku, qty) VALUES ("order1", "sku1", 12)'))
     [[olid]] = session.execute(
-        text('SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku'),
-        dict(orderid='order1', sku='sku1')
+        text("SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku"), dict(orderid="order1", sku="sku1")
     )
     session.execute(
-        text('INSERT INTO batches (reference, sku, _purchased_quantity, eta)'
-        ' VALUES ("batch1", "sku1", 100, null)')
+        text("INSERT INTO batches (reference, sku, _purchased_quantity, eta)" ' VALUES ("batch1", "sku1", 100, null)')
     )
     [[bid]] = session.execute(
-        text('SELECT id FROM batches WHERE reference=:ref AND sku=:sku'),
-        dict(ref='batch1', sku='sku1')
+        text("SELECT id FROM batches WHERE reference=:ref AND sku=:sku"), dict(ref="batch1", sku="sku1")
     )
     session.execute(
-        text('INSERT INTO allocations (orderline_id, batch_id) VALUES (:olid, :bid)'),
-        dict(olid=olid, bid=bid)
+        text("INSERT INTO allocations (orderline_id, batch_id) VALUES (:olid, :bid)"), dict(olid=olid, bid=bid)
     )
 
     batch = session.query(model.Batch).one()
 
-    assert batch._allocations == {
-        model.OrderLine("order1", "sku1", 12)
-    }
+    assert batch._allocations == {model.OrderLine("order1", "sku1", 12)}
