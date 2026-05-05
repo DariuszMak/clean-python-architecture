308779744205411fda89fc4ea287c389a5221be3
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-05-05 12:07:59 +0200

    fix test_batches

--- tests/unit/test_batches.py
+++ tests/unit/test_batches.py
@@ -12,7 +12,7 @@ def test_allocating_to_a_batch_reduces_the_available_quantity() -> None:
     assert batch.available_quantity == 18
 
 
-def make_batch_and_line(sku, batch_qty, line_qty):
+def make_batch_and_line(sku: str, batch_qty: int, line_qty: int) -> tuple[Batch, OrderLine]:
     return (
         Batch("batch-001", sku, batch_qty, eta=datetime.now(tz=UTC).date()),
         OrderLine("order-123", sku, line_qty),
