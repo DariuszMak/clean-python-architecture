1446bc5cc242688caddca6e9a2d0b96434f84553
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-05-05 12:05:11 +0200

    types for notifications

--- src/allocation/adapters/notifications.py
+++ src/allocation/adapters/notifications.py
@@ -1,24 +1,29 @@
 import abc
 import smtplib
+from typing import Any
 
 from allocation import config
 
 
 class AbstractNotifications(abc.ABC):
     @abc.abstractmethod
-    def send(self, destination, message):
+    def send(self, destination: str, message: str) -> None:
         raise NotImplementedError
 
 
-DEFAULT_HOST = config.get_email_host_and_port()["host"]
-DEFAULT_PORT = config.get_email_host_and_port()["port"]
+DEFAULT_HOST: str = config.get_email_host_and_port()["host"]
+DEFAULT_PORT: int = config.get_email_host_and_port()["port"]
 
 
 class EmailNotifications(AbstractNotifications):
-    def __init__(self, smtp_host=DEFAULT_HOST, port=DEFAULT_PORT):
-        self.server = smtplib.SMTP(smtp_host, port=port)
+    def __init__(self, smtp_host: str = DEFAULT_HOST, port: int = DEFAULT_PORT) -> None:
+        self.server: smtplib.SMTP = smtplib.SMTP(smtp_host, port=port)
         self.server.noop()
 
-    def send(self, destination, message):
-        msg = f"Subject: allocation service notification\n{message}"
-        self.server.sendmail(from_addr="allocations@example.com", to_addrs=[destination], msg=msg)
+    def send(self, destination: str, message: str) -> None:
+        msg: str = f"Subject: allocation service notification\n{message}"
+        self.server.sendmail(
+            from_addr="allocations@example.com",
+            to_addrs=[destination],
+            msg=msg,
+        )
