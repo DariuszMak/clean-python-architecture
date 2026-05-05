dbaef637a95e25566f1c2497552bdaf93dcb4d6e
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-05-05 12:21:30 +0200

    fix redis eventpublisher

--- src/allocation/adapters/redis_eventpublisher.py
+++ src/allocation/adapters/redis_eventpublisher.py
@@ -1,7 +1,7 @@
 import json
 import logging
 from dataclasses import asdict
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, cast
 
 import redis
 
@@ -12,9 +12,9 @@ if TYPE_CHECKING:
 
 logger = logging.getLogger(__name__)
 
-r = redis.Redis(**config.get_redis_host_and_port())
+r = redis.Redis(**cast(dict[str, Any], config.get_redis_host_and_port()))
 
 
-def publish(channel, event: events.Event):
+def publish(channel: str, event: "events.Event") -> None:
     logging.info("publishing: channel=%s, event=%s", channel, event)
-    r.publish(channel, json.dumps(asdict(event)))
+    r.publish(channel, json.dumps(asdict(cast(Any, event))))
