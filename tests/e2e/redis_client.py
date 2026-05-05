d5bc7cb2ac8d1d8af754c95815bd162074aa20df
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-05-05 12:14:32 +0200

    redis client fix

--- tests/e2e/redis_client.py
+++ tests/e2e/redis_client.py
@@ -1,19 +1,23 @@
 import json
+from typing import Any
 
 import redis
 
 from allocation import config
 
-r = redis.Redis(**config.get_redis_host_and_port())
+get_redis_host_and_port = config.get_redis_host_and_port
+
+r = redis.Redis(**get_redis_host_and_port())
+r_any: Any = r
 
 
-def subscribe_to(channel):
-    pubsub = r.pubsub()
+def subscribe_to(channel: str) -> Any:
+    pubsub = r_any.pubsub()
     pubsub.subscribe(channel)
     confirmation = pubsub.get_message(timeout=3)
     assert confirmation["type"] == "subscribe"
     return pubsub
 
 
-def publish_message(channel, message):
-    r.publish(channel, json.dumps(message))
+def publish_message(channel: str, message: Any) -> None:
+    r_any.publish(channel, json.dumps(message))
