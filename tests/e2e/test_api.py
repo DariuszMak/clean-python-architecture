db2f577a886b51bb7ff64df03dd332fb7c3d4ed8
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-05-06 08:34:52 +0200

    update test api

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -9,9 +9,10 @@ from tests.random_refs import random_batchref, random_orderid, random_sku
 def test_happy_path_returns_202_and_batch_is_allocated() -> None:
     orderid = random_orderid()
     sku, othersku = random_sku(), random_sku("other")
-    earlybatch = random_batchref(1)
-    laterbatch = random_batchref(2)
-    otherbatch = random_batchref(3)
+    earlybatch = random_batchref("1")
+    laterbatch = random_batchref("2")
+    otherbatch = random_batchref("3")
+
     api_client.post_to_add_batch(laterbatch, sku, 100, "2011-01-02")
     api_client.post_to_add_batch(earlybatch, sku, 100, "2011-01-01")
     api_client.post_to_add_batch(otherbatch, othersku, 100, None)
@@ -30,6 +31,7 @@ def test_happy_path_returns_202_and_batch_is_allocated() -> None:
 @pytest.mark.usefixtures("restart_api")
 def test_unhappy_path_returns_400_and_error_message() -> None:
     unknown_sku, orderid = random_sku(), random_orderid()
+
     r = api_client.post_to_allocate(
         orderid,
         unknown_sku,
