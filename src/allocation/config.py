42c3d39f9cbb0e51db8b332a858ba679d3b3f2cc
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-05-11 16:01:55 +0200

    Revert "update function get_postgres_uri"
    This reverts commit f42811e92fe839d98deef65ac08b81c010bdc68d.

--- src/allocation/config.py
+++ src/allocation/config.py
@@ -16,12 +16,7 @@ class EmailConfig(TypedDict):
 def get_postgres_uri() -> str:
     host = os.environ.get("DB_HOST", "localhost")
     port = 54321 if host == "localhost" else 5432
-
-    if os.environ.get("PYTEST_CURRENT_TEST"):
-        password = os.environ.get("DB_PASSWORD", "abc123")
-    else:
-        password = os.environ["DB_PASSWORD"]
-
+    password = os.environ.get("DB_PASSWORD", "abc123")
     user, db_name = "allocation", "allocation"
 
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
