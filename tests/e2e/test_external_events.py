248f3c95e9fc0e84955f00833c56e5b8c3bb556a
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-05-19 20:00:33 +0200

    fix the test to be ok on windows

--- tests/e2e/test_external_events.py
+++ tests/e2e/test_external_events.py
@@ -15,6 +15,9 @@ def test_change_batch_quantity_leading_to_reallocation() -> None:
 
     order_id, stock_keeping_unit = random_order_id(), random_stock_keeping_unit()
     earlier_batch, later_batch = random_batch_reference("old"), random_batch_reference("newer")
+
+    subscription = subscribe_to("line_allocated")
+
     post_to_add_batch(earlier_batch, stock_keeping_unit, quantity=10, estimated_time_of_arrival="2011-01-02")
     post_to_add_batch(later_batch, stock_keeping_unit, quantity=10, estimated_time_of_arrival="2011-01-03")
     r = post_to_allocate(order_id, stock_keeping_unit, 10)
@@ -22,12 +25,10 @@ def test_change_batch_quantity_leading_to_reallocation() -> None:
     response = get_allocation(order_id)
     assert response.json()[0]["batch_reference"] == earlier_batch
 
-    subscription = subscribe_to("line_allocated")
-
     publish_message("change_batch_quantity", {"batch_reference": earlier_batch, "quantity": 5})
 
     messages = []
-    for attempt in Retrying(stop=stop_after_delay(3), reraise=True):
+    for attempt in Retrying(stop=stop_after_delay(10), reraise=True):
         with attempt:
             message = subscription.get_message(timeout=1)
             if message:
