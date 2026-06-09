aadb13d4e84fa7270173bfed42c244024305d02e
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-09 22:36:39 +0200

    use cast

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -1,6 +1,6 @@
 import inspect
 from collections.abc import Callable
-from typing import Any
+from typing import Any, cast
 
 from src.adapters.notifications import AbstractNotifications, EmailNotifications
 from src.adapters.orm import start_mappers
@@ -47,10 +47,8 @@ def bootstrap(
 
     bus_unit_of_work: AbstractUnitOfWork | None = unit_of_work
 
-    assert bus_unit_of_work is not None
-
     return MessageBus(
-        unit_of_work=bus_unit_of_work,
+        unit_of_work=cast("AbstractUnitOfWork", bus_unit_of_work),
         event_handlers=injected_event_handlers,
         command_handlers=injected_command_handlers,
     )
