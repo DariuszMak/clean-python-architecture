5f3250b53222505d0753a64d46861518d5c495bc
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-09 19:09:35 +0200

    autoformat

--- src/allocation/bootstrap.py
+++ src/allocation/bootstrap.py
@@ -5,10 +5,10 @@ from typing import Any
 from src.adapters.notifications import AbstractNotifications, EmailNotifications
 from src.adapters.orm import start_mappers
 from src.adapters.redis_eventpublisher import publish
+from src.helpers.logging_setup import logging_setup
 from src.service_layer.handlers import COMMAND_HANDLERS, EVENT_HANDLERS
 from src.service_layer.messagebus import MessageBus
 from src.service_layer.unit_of_work import AbstractUnitOfWork, SqlAlchemyUnitOfWork
-from src.helpers.logging_setup import logging_setup
 
 PublishCallable = Callable[..., Any]
 HandlerCallable = Callable[..., Any]
