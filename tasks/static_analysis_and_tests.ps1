1a3cc208ec37a9485922461108437f1d2e53d04e
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-08-17 19:37:52 +0200

    enchance the docker script

--- tasks/static_analysis_and_tests.ps1
+++ tasks/static_analysis_and_tests.ps1
@@ -1,7 +1,7 @@
 .\scripts\format_and_lint.ps1 ; 
 
 docker-compose up -d --build --remove-orphans ; 
-uv run pytest tests/ --cov=src --cov-report=html --cov-report=xml --cov-config=.coveragerc -vv ; 
+docker-compose run --rm --no-deps --entrypoint=uv api run pytest /tests --cov=src --cov-report=html --cov-report=xml --cov-config=.coveragerc -vv ; 
 docker-compose logs --tail=25 api kafka_eventconsumer ; 
 
 Start-Process .\htmlcov\index.html ; 
