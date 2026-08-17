ed4974e73d2c537dd852cbf3470b793df2cb3327
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-08-17 19:06:03 +0200

    remove redundancy script

--- tasks/static_analysis_and_tests.ps1
+++ tasks/static_analysis_and_tests.ps1
@@ -1,15 +1,8 @@
 .\scripts\format_and_lint.ps1 ; 
 
-docker-compose down --remove-orphans ; 
-docker-compose build ; 
-docker-compose up -d ; 
-docker-compose run --rm --no-deps --entrypoint=pytest api /tests ; 
-docker-compose logs --tail=25 api kafka_eventconsumer ; 
-
-docker-compose down --remove-orphans ; 
-docker-compose build ; 
-docker-compose up -d ; 
+docker-compose up -d --build --remove-orphans ; 
 uv run pytest tests/ --cov=src --cov-report=html --cov-report=xml --cov-config=.coveragerc -vv ; 
 docker-compose logs --tail=25 api kafka_eventconsumer ; 
 
 Start-Process .\htmlcov\index.html ; 
+Start-Process "http://127.0.0.1:5005" ; 
